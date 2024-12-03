9456788dc99472fea948c76237d689b2fd7f96a2
Evan Scott <evan.scott@dc.rr.com>
2024-12-03 00:00:45 -0800

    Add basic table reading for dataset

--- Tyler_Code.R
+++ Tyler_Code.R
@@ -1,1 +1,7 @@
 # Ty
+departments <- read.csv('departments.csv')
+aisles <- read.csv('aisles.csv')
+order_products_prior <- read.csv('order_products__prior.csv')
+order_products_train <- read.csv('order_products__train.csv')
+orders <- read.csv('orders.csv')
+products <- read.csv('products.csv')
