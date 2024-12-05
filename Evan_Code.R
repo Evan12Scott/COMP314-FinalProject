4416134c165bd5dfbf062c15a13c401ce8898f51
Evan Scott <evan.scott@dc.rr.com>
2024-12-05 14:23:25 -0800

    Cleaning of tables complete

--- Evan_Code.R
+++ Evan_Code.R
@@ -1,7 +1,41 @@
+library(dplyr)
 # Evan
 departments <- read.csv('departments.csv')
 aisles <- read.csv('aisles.csv')
-order_products_prior <- read.csv('order_products__prior.csv')
 order_products_train <- read.csv('order_products__train.csv')
 orders <- read.csv('orders.csv')
 products <- read.csv('products.csv')
+
+# Cleaning
+
+# Remove eval_set column because not relevant
+orders_cleaned <- orders_cleaned %>%
+  select(-eval_set)
+str(orders_cleaned)
+
+# Check and handle NAs in tables
+sum(is.na(departments))
+sum(is.na(aisles))
+sum(is.na(order_products_prior))
+sum(is.na(order_products_train))
+sum(is.na(orders))
+sum(is.na(products))
+
+orders_cleaned <- na.omit(orders)
+sum(is.na(orders_cleaned))
+
+# Table structure and merge aisles and departments tables into the tables that reference them to reduce table count
+str(aisles)
+str(departments)
+str(orders_cleaned)
+str(products)
+str(order_products_train)
+
+# Products is the only table which references aisles and departments so merge aisles and department tables into it
+products_cleaned <- products %>%
+  left_join(departments, by = "department_id") %>%
+  left_join(aisles, by = "aisle_id") %>%
+  select(-department_id, -aisle_id)
+
+str(products_cleaned) # No longer need to use aisles or departments tables as both referenced now in products
+
