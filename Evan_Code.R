d0695260abf250a2363166a21ead8e1000938fcf
Evan Scott <evan.scott@dc.rr.com>
2024-12-15 12:37:37 -0800

    Finish data preprocessing and cleaning

--- Evan_Code.R
+++ Evan_Code.R
@@ -1,14 +1,16 @@
 library(dplyr)
 library(tidyverse)
-# Evan
+library(sqldf)
+
 departments <- read.csv('departments.csv')
 aisles <- read.csv('aisles.csv')
 order_products_train <- read.csv('order_products__train.csv')
 orders <- read.csv('orders.csv')
 products <- read.csv('products.csv')
 
-# Cleaning
-
+######################
+###### Cleaning ######
+######################
 
 # Check and handle NAs in tables
 sum(is.na(departments))
@@ -18,6 +20,9 @@ sum(is.na(order_products_train))
 sum(is.na(orders))
 sum(is.na(products))
 
+# Cannot confirm the NA values in 'days_since_prior_order' column indicate a customer's
+# first time ordering thus we drop those records to avoid false/incorrect imputations since
+# < 5% of total records in this table
 orders_cleaned <- na.omit(orders)
 sum(is.na(orders_cleaned))
 
@@ -41,3 +46,33 @@ products_cleaned <- products %>%
 
 str(products_cleaned) # No longer need to use aisles or departments tables as both referenced now in products
 
+# Add columns for time frames (day of the week and hour of the day)
+orders_cleaned <- orders_cleaned %>% mutate(
+  order_day = case_when(
+    order_dow == 0 ~ 'Sunday',
+    order_dow == 1 ~ 'Monday',
+    order_dow == 2 ~ 'Tuesday',
+    order_dow == 3 ~ 'Wednesday',
+    order_dow == 4 ~ 'Thursday',
+    order_dow == 5 ~ 'Friday',
+    order_dow == 6 ~ 'Saturday'
+  ),
+  order_hour = case_when(
+    order_hour_of_day ==  0 ~ '12AM',
+    order_hour_of_day == 12 ~ '12PM',
+    order_hour_of_day < 12 ~ paste0(order_hour_of_day, 'AM'),
+    order_hour_of_day > 12 ~ paste0(order_hour_of_day - 12, 'PM')
+  )
+)
+
+#######################################
+###### Queries and Visualization ######
+#######################################
+
+# Visualization 1
+
+# Visualization 2
+
+# Visualization 3
+
+# Visualization 4
